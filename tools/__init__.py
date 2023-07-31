25a9d048f8b41e424f72d4cf1c59f6bb39fb110d
Pedro Algarvio <palgarvio@vmware.com>
2023-07-31 12:37:25 +0100

    Update workflows
    Signed-off-by: Pedro Algarvio <palgarvio@vmware.com>

---
+++ tools/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+from __future__ import annotations
+
+import ptscripts
+
+ptscripts.register_tools_module("tools.pre_commit")
